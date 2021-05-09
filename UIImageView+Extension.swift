da56d422a7557ffb7e2499a445a65e9945924170
Shanmugarajeshwaran R <47396623+Nova-Corp@users.noreply.github.com>
2021-05-09 17:37:15 +0530

    NSCache Added for Caching Images.

--- UIImageView+Extension.swift
+++ UIImageView+Extension.swift
@@ -8,23 +8,37 @@
 
 import UIKit
 
+let imageCache: NSCache<NSString, UIImage> = NSCache<NSString, UIImage>()
+
 extension UIImageView {
-    func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+    
+    func setImage(from url: URL,
+                  contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        
         contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() { [weak self] in
-                self?.image = image
-            }
-        }.resume()
+        if let imageCached = imageCache.object(forKey: url.absoluteString as NSString) {
+            image = imageCached
+            return
+        }else{
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                guard
+                    let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                    let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                    let data = data, error == nil,
+                    let image = UIImage(data: data)
+                    else { return }
+                DispatchQueue.main.async() { [weak self] in
+                    self?.image = image
+                    imageCache.setObject(image, forKey: url.absoluteString as NSString)
+                }
+            }.resume()
+        }
     }
-    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+    func setImage(from link: String,
+                  contentMode mode: UIView.ContentMode = .scaleAspectFit,
+                  cache: Bool = true) {
         guard let url = URL(string: link) else { return }
-        downloaded(from: url, contentMode: mode)
+        setImage(from: url, contentMode: mode)
     }
 }
+
