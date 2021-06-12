bc7d2e5b8bf3aebdecf04e22f8b1637bd53dce1b
Shanmugarajeshwaran R <47396623+Nova-Corp@users.noreply.github.com>
2021-06-12 21:18:11 +0530

    Create UITextField+Extension.swift

---
+++ UITextField+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  UITextField+Extension.swift
+//  Local-Notification-Practice
+//
+//  Created by ADMIN on 12/06/21.
+//  Copyright © 2021 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(UITextField.resignFirstResponder))
+
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+
+        self.inputAccessoryView = doneToolbar
+    }
+}
